91485422c757aab817a32ad8b02db18180a6041d
unknown <hayyana72@gmail.com>
2026-07-16 00:42:40 -0700

    disable medusa admin

--- apps/backend/medusa-config.ts
+++ apps/backend/medusa-config.ts
@@ -12,9 +12,5 @@ module.exports = defineConfig({
       jwtSecret: process.env.JWT_SECRET,
       cookieSecret: process.env.COOKIE_SECRET,
     }
-  },
-
-  admin: {
-    backend_only: true,
-  },
+  }
 })
